e92c6d82813b6668223673483101019f352df2cc
es2055 <4780292+es2055@users.noreply.github.com>
2018-09-07 00:55:11 -0700

    added route util for auth routes

---
+++ jobCentWeb/Frontend/src/util/route_util.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+
+const Auth = ({component: Component, path, loggedIn}) => (
+  <Route path={path} render={(props) => (
+      !loggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    )} />
+);
+
+const Protected = ({component: Component, path, loggedIn}) => (
+  <Route path={path} render={(props) => {
+
+      if (loggedIn) {
+        return <Component {...props} />;
+
+      } else {
+        // origin = path;
+        return <Redirect to="/login" />;
+      }
+    }} />
+);
+
+const mapStateToProps = state => {
+    // state.origin = null;
+  return ({
+    loggedIn: Boolean(state.session.currentUser),
+    // origin: state.origin
+  });
+};
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
