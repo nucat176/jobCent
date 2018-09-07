13e7352ecece3af8484c371d087f5bd6ebcf6d08
es2055 <4780292+es2055@users.noreply.github.com>
2018-09-07 01:02:38 -0700

    setting up routes

--- jobCentWeb/Frontend/src/App.js
+++ jobCentWeb/Frontend/src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import Landing from './components/landing';
-import { HashRouter, Route, Switch } from "react-router-dom";
-import './scss/App.css';
-import Test from './components/test';
+import React, { Component } from "react";
+import Landing from "./components/landing";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Route, Switch } from "react-router-dom";
+import "./scss/App.css";
+import Test from "./components/test";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/login" component={Test} />
+        <Switch>
+          <ProtectedRoute path="/dashboard" component={Test} />
+          <AuthRoute path="/login" component={Test} />
+          <Route exact path="/" component={Landing} />
+        </Switch>
       </div>
     );
   }
